e637799ab5cbb524f3e4670b53b75435ff7cbfcc
chenyili0818 <lichenyi@stu.pku.edu.cn>
2025-10-27 14:37:46 +0800

    update

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
-python gen.py \
-  --table /pfs/lichenyi/station/VLMEval/outputs/mme/UnifyModelEval/train_qwendit_unify_interleave_stage1p5/0000052000/UnifyModelEval_MME_auxmatch.xlsx \
+python /pfs/lichenyi/und_caption/gen.py \
+  --table /pfs/lichenyi/station/VLMEval/outputs/mme/UnifyModelEval/train_qwendit_unify_interleave_stage2/0000016000/UnifyModelEval_MME.xlsx \
   --src-image-root /pfs/shared_eval/datasets/images/MME \
   --output-dir /pfs/lichenyi/und_caption \
-  --tag train_qwendit_unify_interleave_stage1p5 \
+  --tag train_qwendit_unify_interleave_stage2 \
   --iter 52000 \
   --per-class 30 \
